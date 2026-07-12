86202b404648a224581f2b4ff45efaf9f2c598b4
runsworth <r.k.f.unsworth@swansea.ac.uk>
2026-07-12 23:47:11 +0100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAXAgHcF_SBhYzgSHOO8-UO_eq23To_qIU",
+  authDomain: "seagrassgame.firebaseapp.com",
+  databaseURL: "https://seagrassgame-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "seagrassgame",
+  storageBucket: "seagrassgame.firebasestorage.app",
+  messagingSenderId: "70641183022",
+  appId: "1:70641183022:web:bc2c8664ff123ab46ef2bf",
+  measurementId: "G-RNVDEPT2YT"
+};
+
+
+
